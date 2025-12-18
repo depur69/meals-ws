a373f0da6d8d6908f6f664b2838873c77b2c87f1
depur69 <depurrr69@gmail.com>
2025-12-18 08:43:28 +0800

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Meal Receipts</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Meal Receipts</h1>
+  </header>
+  <main>
+    <section id="meal-index">
+      <div class="meal-letters">
+
+      </div>
+      <div class="meal-search">
+        
+      </div>
+    </section>
+  </main>
+</body>
+</html>
